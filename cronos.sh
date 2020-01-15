9e92b27e19c226977f76b52f41ccf2f8b2ec0682
wirusmod <wirusmod@gmail.com>
2020-01-15 22:19:23 +0100

    fix path and change version

--- cronos.sh
+++ cronos.sh
@@ -38,8 +38,8 @@ CR_KERNEL=$CR_DIR/arch/arm64/boot/Image
 # Compiled dtb by dtbtool
 CR_DTB=$CR_DIR/boot.img-dtb
 # Kernel Name and Version
-CR_VERSION=V4.0
-CR_NAME=CronosKernel
+CR_VERSION=V1.0
+CR_NAME=WirusMOD-CronosKernel
 # Thread count
 CR_JOBS=$(nproc --all)
 # Target Android version
